d5276836485f5e84381520da057a37d3a3cbd14c
alec-gironda <alec.gironda@gmail.com>
2022-07-15 16:58:12 -0400

    testing stuff

--- testing_NNs.py
+++ testing_NNs.py
@@ -5,12 +5,29 @@ import numpy as np
 import math
 import time
 
-#imported data must be in (x_train,y_train),(x_test,y_test) = data format
+
+def calculate_runtime(func):
+    '''
+    decorator to calculate the runtime of functions
+    while still returning their output
+    '''
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+
+        out = func(*args,**kwargs)
+
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+        return out
+    return wrapper
+
 
 class Model:
     '''
     takes information on dataset and neural network parameters.
     compiles a model.
+
+    imported data must be in (x_train,y_train),(x_test,y_test) = data format
 
 
     can try different optimizers
@@ -20,7 +37,7 @@ class Model:
 
     def __init__(self,dataset,hidden_units,layers,training_size,
                 learning_rate,decay_lr,dropout,dropout_size,epochs,
-                batch_size,loss,metrics):
+                batch_size,loss,metrics,activation):
 
         #read in inputs
         self.dataset = dataset
@@ -35,23 +52,32 @@ class Model:
         self.batch_size = batch_size
         self.loss = loss
         self.metrics = metrics
+        self.activation = activation
 
         processed_data = self.process_dataset_input(self.dataset)
 
-        self.x_train = tf.keras.utils.normalize(processed_data[0][:self.training_size+1],axis=1)
-        self.y_train = processed_data[1][:self.training_size+1]
+        self.x_train = tf.keras.utils.normalize(processed_data[0][:self.training_size],axis=1)
+        self.y_train = processed_data[1][:self.training_size]
 
         self.steps_per_epoch = len(self.x_train)//self.batch_size
 
-        self.x_test = tf.keras.utils.normalize(processed_data[2][:self.training_size+1],axis=1)
+        self.x_test = tf.keras.utils.normalize(processed_data[2],axis=1)
         self.y_test = processed_data[3]
 
         self.compiled_model = self.compile_model(self.hidden_units,
                                                  self.learning_rate,
                                                  self.dropout,self.dropout_size,
-                                                 self.loss,self.metrics)
+                                                 self.loss,self.metrics,self.activation,
+                                                 self.layers)
 
     def process_dataset_input(self,dataset):
+        '''
+
+        processes data with [2,2] shape
+
+        (x_train,y_train),(x_test,y_test) = data
+
+        '''
 
         x_train = dataset[0][0]
         y_train = dataset[0][1]
@@ -62,16 +88,24 @@ class Model:
         return [x_train,y_train,x_test,y_test]
 
     def compile_model(self,hidden_units,learning_rate,
-                      dropout,dropout_size,loss,metrics):
+                      dropout,dropout_size,loss,metrics,activation,layers):
 
-        #add layers, but start with one for now
+        '''
+        the actual architecture of the model
+
+        '''
 
 
         model = tf.keras.models.Sequential()
         model.add(tf.keras.layers.Flatten())
-        model.add(tf.keras.layers.Dense(hidden_units,activation=tf.nn.relu))
-        if dropout:
-            model.add(tf.keras.layers.Dropout(dropout_size))
+        curr_hidden_units = hidden_units
+        for layer in range(layers):
+            model.add(tf.keras.layers.Dense(curr_hidden_units,activation=activation))
+            if dropout:
+                model.add(tf.keras.layers.Dropout(dropout_size))
+                curr_hidden_units //=2
+                if curr_hidden_units <10:
+                    curr_hidden_units = 10
         model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
 
         optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
@@ -84,7 +118,7 @@ class Model:
 class Train:
     '''
 
-    Trains the model on the data.
+    trains the model on the training data
 
     '''
 
@@ -98,13 +132,17 @@ class Train:
         self.trained_model = self.fit_model(self.compiled_model,self.model)
 
     def lr_decay_function(self):
+        '''
+        function to decay the learning rate as epoch number increases
+
+        '''
         return [tf.keras.callbacks.LearningRateScheduler(lambda epoch: 0.01 *
                                                         math.pow(0.6,epoch),
                                                         verbose=True)]
+    @calculate_runtime
     def fit_model(self,compiled_model,model):
-        compiled_model.fit(model.x_train,model.y_train,epochs=model.epochs,
-                  steps_per_epoch = model.steps_per_epoch,
-                  callbacks=self.decay_lr)
+        compiled_model.fit(model.x_train,model.y_train,epochs=model.epochs,steps_per_epoch = model.steps_per_epoch,
+                                                callbacks=self.decay_lr)
 
         return compiled_model
 
@@ -152,40 +190,29 @@ class Plot:
 if __name__ == "__main__":
 
     #check CPU performance vs GPU
-
-    os.environ["CUDA_VISIBLE_DEVICES"]="-1"
-
-    start_time = time.time()
+    #os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
     mnist = tf.keras.datasets.mnist
     mnist = mnist.load_data()
 
-    #(self,dataset,hidden_units,layers,training_size,
-    #learning_rate,decay_lr,dropout,dropout_size,epochs,
-    #batch_size,loss,metrics):
+    #hidden_units_list = [16*i for i in range(784//16) if i!=0]
+    layer_list = np.arange(1,7)
+    val_acc_list = []
+    for layer in layer_list:
 
-    # hidden_units_list = [200]
-    # #hidden_units_list = [16*i for i in range(784//16) if i!=0]
-    # val_acc_list = []
-    # for hidden_units in hidden_units_list:
+        #(self,dataset,hidden_units,layers,training_size,
+        #learning_rate,decay_lr,dropout,dropout_size,epochs,
+        #batch_size,loss,metrics):
 
-    compiled_model = Model(mnist,200,1,60000,0.01,False,True,0.25,10,100,
-                 'sparse_categorical_crossentropy',['accuracy'])
+        compiled_model = Model(mnist,200,layer,60000,0.01,True,False,0.25,10,100,
+                     'sparse_categorical_crossentropy',['accuracy'],tf.nn.relu)
 
-    trained_model = Train(compiled_model)
-    trained_model = trained_model.trained_model
+        trained_model = Train(compiled_model)
+        trained_model = trained_model.trained_model
 
-    evaluate = Evaluate(compiled_model,trained_model)
+        evaluate = Evaluate(compiled_model,trained_model)
 
-    print(evaluate.val_acc)
+        val_acc_list.append(evaluate.val_acc)
 
-    #print time it took to execute program
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    #24 sec on gpu
-    #24.5sec on cpu
-
-    # val_acc_list.append(evaluate.val_acc)
-
-    # hidden_units_plot = Plot(hidden_units_list,val_acc_list,"hidden units","accuracy")
-    # hidden_units_plot.scatter_plot()
+    training_size_plot = Plot(layer_list,val_acc_list,"number of layers","accuracy")
+    training_size_plot.scatter_plot()
